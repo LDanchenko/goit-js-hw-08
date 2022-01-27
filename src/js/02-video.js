68007395c98ac3b163ed0d042eb14faa3244d072
Lubov <2l-u-b-o-v@ukr.net>
2022-01-27 15:06:22 +0200

    task02 - подключает библиотеку Vimeo и получает время воспроизведения

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function () {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      console.log(seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
