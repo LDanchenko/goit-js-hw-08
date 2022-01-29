fa81809e93ed815b71f55a8291696a3e5faa0f0d
Lubov <2l-u-b-o-v@ukr.net>
2022-01-29 22:20:14 +0200

    task03 - отслеживает ввод данных, сохраняет в веб-хранилище

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const form = document.querySelector('.feedback-form');
+console.log(form);
+const data = {
+  email: '',
+  message: '',
+};
 
+form.addEventListener('input', event => {
+  data[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+});
